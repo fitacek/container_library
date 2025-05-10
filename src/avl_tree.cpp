7cc50f71a08f46ec8aa35b6d7332aaf1a0b1ba97
fitacek <fitacek@protonmail.com>
2025-05-10 16:13:35 +0200

    avl tree: autoformat avl_tree.cpp

--- src/avl_tree.cpp
+++ src/avl_tree.cpp
@@ -1,47 +1,57 @@
 #include "avl_tree.hpp"
 
-AVLTree::AVLTree(){}
+AVLTree::AVLTree() {}
 
-size_t AVLTree::size() const{
+size_t AVLTree::size() const
+{
     return nodeCount;
 }
 
-bool AVLTree::empty() const {
+bool AVLTree::empty() const
+{
     return nodeCount == 0;
 }
 
-bool AVLTree::insert(const int key, const int value){
-    if (nodeCount == 0){
+bool AVLTree::insert(const int key, const int value)
+{
+    if (nodeCount == 0)
+    {
         root = std::make_unique<Node>(key, value);
         nodeCount++;
         return true;
     }
 
-    Node* head = root.get();
-    Node* tail = nullptr;
+    Node *head = root.get();
+    Node *tail = nullptr;
 
-    while (head != nullptr){
+    while (head != nullptr)
+    {
         tail = head;
-        if (key < head->key){
+        if (key < head->key)
+        {
             // go left
             head = head->left.get();
         }
-        else if (key > head->key){
+        else if (key > head->key)
+        {
             // go right
             head = head->right.get();
         }
-        else {
+        else
+        {
             // trying to insert duplicate key
             return false;
         }
     }
     // head == nullptr
     // tail == parrent of head
-    if (key < tail->key){
+    if (key < tail->key)
+    {
         // insert new node to left
         tail->left = std::make_unique<Node>(key, value, tail);
     }
-    else{
+    else
+    {
         // insert new node to right
         tail->right = std::make_unique<Node>(key, value, tail);
     }
@@ -49,10 +59,9 @@ bool AVLTree::insert(const int key, const int value){
     // node is inserted, now there may be need to rebalance the tree
 }
 
+AVLTree::Node::Node(int key, int value, Node *parrent) : key(key), value(value), parrent(parrent) {}
 
-AVLTree::Node::Node(int key, int value, Node * parrent) : key(key), value(value), parrent(parrent) {}
-
-bool AVLTree::Node::isLeaf() const{
+bool AVLTree::Node::isLeaf() const
+{
     return left == nullptr && right == nullptr;
 }
-
