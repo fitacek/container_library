99f820071d02f799888e76b164b3b6e313df8f5b
fitacek <fitacek@protonmail.com>
2025-05-10 15:12:08 +0200

    avl tree: add header module

---
+++ include/avl_tree.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <optional>
+#include <memory>
+
+// naive aproach with int as key and value.
+// Will make it templated later
+class AVLTree {
+
+public:
+    AVLTree();
+   // ~AVLTree(){}
+    bool insert(const int key, const int value);
+    bool remove(const int key);
+
+    void contains(const int key) const;
+    std::optional<int>find(const int key) const;
+
+    bool empty() const;
+    size_t size() const;
+    void clear();
+
+private:
+    struct Node {
+        int key;
+        int value;
+        std::unique_ptr<Node> left;
+        std::unique_ptr<Node> right;
+        std::unique_ptr<Node> parrent;
+    };
+    std::unique_ptr<Node> root;
+    size_t nodeCount = 0;
+};
