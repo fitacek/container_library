44d69fbe7d72e493ef15bbd7070e58c9b2198853
fitacek <fitacek@protonmail.com>
2025-05-11 09:17:19 +0200

    include cpp file to make ide debuggger work

--- test/test_avl_tree.cpp
+++ test/test_avl_tree.cpp
@@ -1,4 +1,5 @@
 #include "../include/avl_tree.hpp"
+#include "../src/avl_tree.cpp" // to make IDE debugger work
 #include <iostream>
 using namespace std;
 
@@ -6,11 +7,15 @@ int main(){
     AVLTree t = AVLTree();
     cout<<t.size()<<endl;
 
-    t.insert(4,0);
-    t.insert(2,0);
-    t.insert(3,0);
+    // t.insert(4,0);
+    // t.insert(2,0);
+    // t.insert(3,0);
+    // t.insert(7,0);
+    // t.insert(6,0);
+    // t.insert(8,0);
+
+    t.insert(5,0);
     t.insert(7,0);
-    t.insert(6,0);
     t.insert(8,0);
 
     t.printInOrder();
